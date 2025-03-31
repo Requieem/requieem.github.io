f5874beaf808b6dd863cfca3ae8770d05a3837a0
Requieem <43446747+Requieem@users.noreply.github.com>
2025-03-31 16:36:44 +0200

    implemented ContentTile.jsx

---
+++ src/Components/ContentTile.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function ContentTile (props) {
+  return (
+    <div onClick={props.onClick}
+         className={'group w-full sm:w-1/3 h-fit overflow-clip p-0.5 hover:scale-95 transition duration-200 ease-out not-hover:drop-shadow-2xl grid grid-cols-1 grid-rows-1'}>
+      <div
+        className={'pointer-events-none col-start-1 col-end-2 row-start-1 row-end-2 border-2 border-oxford-blue grid grid-rows-5 grid-cols-4 aspect-2/1 rounded-lg overflow-clip justify-stretch items-center'}>
+        <img className={'w-full row-start-1 row-end-5 col-start-1 col-end-6 object-cover'} src={props.src}
+             alt={props.alt}/>
+        <div
+          className={'group-hover:opacity-0 transition-all duration-200 ease-in-out h-full w-full col-span-full row-span-full flex flex-col overflow-clip justify-center items-center'}>
+          <div
+            className={'row-start-2 row-end-3 col-start-1 col-end-5 bg-gradient-to-tl  from-oxford-blue to-yinmn-blue flex items-center justify-center rounded-md p-1 pr-5 pl-5 w-fit justify-self-center mb-0.5'}>
+            <h3 className={'text-center text-text-light font-semibold'}>{props.title}</h3>
+          </div>
+          <div
+            className={'row-start-4 row-end-4 col-start-1 col-end-5 bg-gradient-to-tl  from-oxford-blue to-yinmn-blue flex items-center justify-center rounded-md p-1 pr-5 pl-5 w-fit justify-self-center mt-0.5'}>
+            <h3 className={'text-center text-text-light'}>{props.subtitle}</h3>
+          </div>
+        </div>
+        <div
+          className={'z-0 h-full w-full col-span-full row-span-full flex flex-col overflow-clip justify-center items-center gap-1 opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out bg-text-dark/55 backdrop-blur-md'}>
+          <div
+            className={'flex-1 flex items-center justify-end rounded-md p-1 pr-5 pl-5 w-fit items-end'}>
+            <h3 className={'text-center text-text-light font-semibold'}>{props.title}</h3>
+          </div>
+          <div
+            className={'flex-1 flex items-center justify-center rounded-md p-1 pr-5 pl-5 w-fit items-start'}>
+            <h3 className={'text-center text-text-light italic font-light'}>Read More</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContentTile
--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,38 @@ import { createRoot }  from 'react-dom/client'
 import './input.css'
 import Sidebar         from './Components/Sidebar.jsx'
 import SquareContainer from './Components/SquareContainer.jsx'
+import ContentTile     from './Components/ContentTile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className="flex w-screen h-screen flex-row overflow-y-scroll overflow-x-hidden">
+    <div className="flex w-screen flex-row overflow-y-scroll overflow-x-hidden">
       <Sidebar/>
-      <div className={'h-fit flex flex-wrap justify-center items-center'}>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/vite.svg'} alt={'Vite Logo'} hint={'Vite'}/>
-        <SquareContainer src={'/react.svg'} alt={'React Logo'} hint={'React'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
-        <SquareContainer src={'/Unity6.png'} alt={'Unity Logo'} hint={'Unity'}/>
+      <div className={'flex sm:flex-wrap flex-col sm:flex-row justify-center items-start'}>
+        <ContentTile onClick={() => console.log('clicked')} title={'Title'} subtitle={'Subtitle'} src={'/BG.jpg'}
+                     alt={'Placeholder'}
+                     content={''}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
+        <ContentTile title={'Title'} subtitle={'Subtitle'} src={'/cave_mesh.png'} alt={'Placeholder'}
+                     content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}/>
       </div>
     </div>
   </StrictMode>,
